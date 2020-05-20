d29fa89ed93c86602cefb5240b5f4d2b44b4c34c
Kenneth Auchenberg <auchenberg@stripe.com>
2020-05-19 22:06:22 -0700

    Add prompt for local forwarding url when debugging. Fixes #37

--- src/stripeDebugProvider.ts
+++ src/stripeDebugProvider.ts
@@ -2,10 +2,11 @@ import { env } from "vscode";
 import * as path from "path";
 import * as vscode from "vscode";
 import { Telemetry } from "./telemetry";
+import { ImplementationRequest } from "vscode-languageclient";
 
 const telemetry = Telemetry.getInstance();
 
-export class StripeDebugProvider {
+export class StripeDebugProvider implements vscode.DebugConfigurationProvider {
   constructor() {
     vscode.debug.onDidTerminateDebugSession((e: vscode.DebugSession) => {
       if (e.name === `Stripe: Listen`) {
@@ -14,49 +15,78 @@ export class StripeDebugProvider {
     });
   }
 
-  getProvider(): vscode.DebugConfigurationProvider {
-    return {
-      provideDebugConfigurations(
-        folder: vscode.WorkspaceFolder | undefined,
-        token?: vscode.CancellationToken
-      ): vscode.ProviderResult<vscode.DebugConfiguration[]> {
-        return Promise.resolve([
-          {
-            name: "Stripe: Webhooks Forward",
-            type: "stripe",
-            request: "launch",
-            command: "listen",
-            localUrl: "http://localhost:3000/stripe-events",
-          },
-        ]);
+  public async provideDebugConfigurations(
+    folder: vscode.WorkspaceFolder | undefined,
+    token?: vscode.CancellationToken
+  ): Promise<vscode.DebugConfiguration[]> {
+    return Promise.resolve([
+      {
+        name: "Stripe: Webhooks Forward",
+        type: "stripe",
+        request: "launch",
+        command: "listen",
+        localUrl: "http://localhost:3000/stripe-events",
       },
-      resolveDebugConfiguration(
-        folder: vscode.WorkspaceFolder | undefined,
-        config: vscode.DebugConfiguration,
-        token?: vscode.CancellationToken
-      ): vscode.ProviderResult<vscode.DebugConfiguration> {
-        if (config && config.type === "stripe") {
-          if (
-            config.request &&
-            config.request === `launch` &&
-            config.command &&
-            config.command === "listen"
-          ) {
-            telemetry.sendEvent("debug.launch");
+    ]);
+  }
 
-            vscode.commands.executeCommand(
-              `stripe.openWebhooksListen`,
-              config.localUrl,
-              config.events
-            );
+  public async resolveDebugConfiguration(
+    folder: vscode.WorkspaceFolder | undefined,
+    config: vscode.DebugConfiguration,
+    token?: vscode.CancellationToken
+  ): Promise<vscode.DebugConfiguration> {
+    if (config && config.type === "stripe") {
+      if (
+        config.request &&
+        config.request === `launch` &&
+        config.command &&
+        config.command === "listen"
+      ) {
+        telemetry.sendEvent("debug.launch");
+
+        if (!config.localUrl) {
+          let action = await showLocaUrlPrompt();
+          if (action === "Yes") {
+            let localUrl = await vscode.window.showInputBox({
+              prompt: "Enter local server url",
+              placeHolder: "http://localhost:3000",
+            });
+
+            if (localUrl) {
+              config.localUrl = localUrl;
+            }
           }
-        } else {
-          vscode.window.showErrorMessage(
-            "No supported launch config was found."
-          );
         }
-        return;
-      },
+
+        vscode.commands.executeCommand(
+          `stripe.openWebhooksListen`,
+          config.localUrl,
+          config.events
+        );
+      }
+    } else {
+      vscode.window.showErrorMessage("No supported launch config was found.");
+    }
+
+    return {
+      type: "",
+      name: "",
+      request: "",
     };
   }
+}
+
+async function showLocaUrlPrompt() {
+  return new Promise((resolve, reject) => {
+    const input = vscode.window.createQuickPick();
+    input.placeholder = "Do you want to forward traffic to your local server?";
+    input.items = [{ label: "Yes" }, { label: "No" }];
+
+    input.onDidAccept(() => {
+      let value = input.selectedItems[0].label;
+      resolve(value);
+    });
+
+    input.show();
+  });
 }
