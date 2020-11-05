b976303fa6e62e43b358bd362ad064165a8ba8b4
Vincent Cheung <vcheung@stripe.com>
2020-11-04 16:41:31 -0800

    For Stripe API Reference URLs, add lang parameter

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,11 +2,14 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 
 suite('Extension Test Suite', () => {
-  test("Should start extension vscode-stripe", () => {
+  test("Should start extension vscode-stripe", async () => {
     const started = vscode.extensions.getExtension(
       "stripe.vscode-stripe",
     );
     assert.notStrictEqual(started, undefined);
+    if (started) {
+      await started.activate();
+    }
     assert.strictEqual(started && started.isActive, true);
   });
 });
