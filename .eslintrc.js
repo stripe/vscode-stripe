9dcaf31ed47c1395570fa2ed55749b9a912f5172
Vincent Cheung <vcheung@stripe.com>
2020-12-10 13:48:09 -0800

    We do not need semantic information for our eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   extends: ["prettier", "prettier/@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
