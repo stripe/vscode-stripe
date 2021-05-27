0954ed8ad9f5e7713ca2285bd67b6aee173eb5cc
gracegoo-stripe <75757829+gracegoo-stripe@users.noreply.github.com>
2021-05-27 09:56:02 -0700

    Move dotnet server into vscode repo and connect language client (#236)

---
+++ src/stripeDotnetLanguageServer/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OmniSharp.Extensions.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Server;
+using Serilog;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace stripe.LanguageServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // TODO -- don't save to fs? Can we pipe it directly to the vscode output console?
+            Log.Logger = new LoggerConfiguration()
+                        .Enrich.FromLogContext()
+                        .WriteTo.File("/tmp/stripe.LanguageServer-logs.txt", rollingInterval: RollingInterval.Day)
+                        .MinimumLevel.Debug()
+                        .CreateLogger();
+
+            Log.Debug("Creating project..");
+            // TODO -- Get this as an arg from the client.
+            const string projectFile = "/Users/gracegoo/stripe/samples/accept-a-payment-dotnet/server/server.csproj";
+
+            // Without this MSBuild can't find the SDK folder.
+            MSBuildLocator.RegisterDefaults();
+            var workspace = MSBuildWorkspace.Create();
+
+            // We need to register to the failed event to be notified if there were any failures.
+            // https://docs.microsoft.com/en-us/dotnet/standard/events/
+            workspace.WorkspaceFailed += WorkspaceFailed;
+            var project = await workspace.OpenProjectAsync(projectFile);
+
+            Log.Debug("Created project...");
+
+            Log.Debug("Starting language server from the VSCode repo!...");
+            var server = await OmniSharp.Extensions.LanguageServer.Server.LanguageServer.From(options =>
+                options
+                    .WithInput(Console.OpenStandardInput())
+                    .WithOutput(Console.OpenStandardOutput())
+                    .ConfigureLogging(
+                            x => x
+                                .AddSerilog(Log.Logger)
+                                .AddLanguageProtocolLogging()
+                                .SetMinimumLevel(LogLevel.Debug)
+                        )
+                    // .WithHandler<TextDocumentHandler>()
+                    .WithHandler<APIReferenceHandler>()
+                    .WithServices(x => x.AddLogging(b => b.SetMinimumLevel(LogLevel.Trace)))
+                    .WithServices(
+                        services =>
+                        {
+                            services.AddSingleton(
+                                provider =>
+                                {
+                                    var loggerFactory = provider.GetService<ILoggerFactory>();
+                                    var logger = loggerFactory.CreateLogger<WorkspaceManager>();
+                                    return new WorkspaceManager(logger, workspace);
+                                }
+                            );
+                        }
+                    )
+                );
+
+            await server.WaitForExit;
+        }
+
+        private static void WorkspaceFailed(object sender, WorkspaceDiagnosticEventArgs e)
+        {
+            var message = e.Diagnostic.Message;
+            Log.Error("Error while loading workspace: " + message);
+        }
+
+    }
+}
---
+++ src/stripeDotnetLanguageServer/README.md
@@ -0,0 +1,41 @@
+## My test dotnet project
+
+## Developing
+
+### C# Intellisense
+
+If you find that intellisense is not working in this directory, you may have to help the omnisharp extension out a bit due to our project having multiple workspace folders.
+
+1. Open the Command Palette
+1. Write "OmniSharp: Select Project" and press Enter.
+1. Choose stripeDotnetLanguageServer.sln
+
+## Running
+
+```
+# Build
+dotnet build
+
+# Clean output
+dotnet clean
+
+# Run main
+dotnet run
+```
+
+## Documents that are helpful
+
+## Debugging
+
+Log files are locally saved to /tmp/stripe.LanguageServer-logs[date].txt. Ex: /tmp/stripe.LanguageServer-logs20210520.txt
+
+To run the language server end to end:
+
+1. Start the client -- For VSCode this means running the extensionDoes that mean you're using a different distributor for each area? I assume adding a women option to your form wouldn't be that difficult. The alternate questions is, why don't we send out women's swag to men and consider that to be appropriate? development host as you normally would.
+2. In this workspace, hit ".NET Core Attach" -- this will bring up a list of processes, there should just be one that the client started. This step will have to change when we move into the extension repo.
+
+## Helpful Documents
+
+https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.languageserver.protocol?view=visualstudiosdk-2019
+https://microsoft.github.io/language-server-protocol/specifications/specification-3-14/
+https://sharplab.io/ -- Syntax tree crawler
