5680aec0835238d8ff9136d80c7e723e780e2139
gracegoo-stripe <75757829+gracegoo-stripe@users.noreply.github.com>
2021-06-03 15:43:39 -0700

    Ignore .env files and redacted keys (#245)
    The redaction change is targeted to protect against the specific redacted format that Stripe returns which looks something like "sk_live_aa********************1234".
    It does not attempt to detect other potential ways users can modify the keys nor actually validate that the key is real.
    
    This change fixes https://github.com/stripe/vscode-stripe/issues/220 and https://github.com/stripe/vscode-stripe/issues/222

--- test/suite/stripeLinter.test.ts
+++ test/suite/stripeLinter.test.ts
@@ -36,6 +36,28 @@ suite('StripeLinter', () => {
       assert.strictEqual(diagnostics.length, 0);
     });
 
+    test('Editor content is not searched on .env files', async () => {
+      const setting = vscode.Uri.parse('untitled:.env');
+      const doc = await vscode.workspace.openTextDocument(setting);
+
+      await vscode.window
+        .showTextDocument(doc)
+        .then((editor) =>
+          editor.edit((builder) => builder.insert(new vscode.Position(0, 0), 'sk_live_1234')),
+        );
+
+      const isIgnoredStub = sandbox.stub(git, 'isIgnored').resolves(false);
+      const telemetrySpy = sandbox.spy(telemetry, 'sendEvent');
+
+      const linter = new StripeLinter(telemetry, git);
+      await linter.activate();
+
+      const diagnostics = vscode.languages.getDiagnostics(doc.uri);
+      assert.strictEqual(isIgnoredStub.calledOnce, true);
+      assert.strictEqual(telemetrySpy.callCount, 0);
+      assert.strictEqual(diagnostics.length, 0);
+    });
+
     test('Diagnostics are pushed when linter finds offenses', async () => {
       const options = {content: 'I have content with critical data : sk_live_1234'};
       const document = await vscode.workspace.openTextDocument(options);
@@ -59,6 +81,23 @@ suite('StripeLinter', () => {
 
     test('No diagnostics are pushed when file is clean', async () => {
       const options = {content: 'I am a good file'};
+      const document = await vscode.workspace.openTextDocument(options);
+      await vscode.window.showTextDocument(document, {preview: false});
+
+      const isIgnoredStub = sandbox.stub(git, 'isIgnored').resolves(false);
+      const telemetrySpy = sandbox.spy(telemetry, 'sendEvent');
+
+      const linter = new StripeLinter(telemetry, git);
+      await linter.activate();
+      const diagnostics: vscode.Diagnostic[] = vscode.languages.getDiagnostics(document.uri);
+
+      assert.strictEqual(isIgnoredStub.calledOnce, true);
+      assert.strictEqual(telemetrySpy.callCount, 0);
+      assert.strictEqual(diagnostics.length, 0);
+    });
+
+    test('Diagnostics are not pushed when redacted keys are present', async () => {
+      const options = {content: 'I have redacted keys : sk_live_aa********************1234'};
       const document = await vscode.workspace.openTextDocument(options);
       await vscode.window.showTextDocument(document, {preview: false});
 
