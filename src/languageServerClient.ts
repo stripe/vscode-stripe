5784573463e04334692aafa889011b4f2d73f8b2
Grace Goo <gracegoo@stripe.com>
2021-06-15 14:09:05 -0700

    Adding conditional language server startup and passing in project from user workspace

--- src/languageServerClient.ts
+++ src/languageServerClient.ts
@@ -5,20 +5,34 @@ import {
   ErrorAction,
   LanguageClient,
   LanguageClientOptions,
-  RevealOutputChannelOn,
   ServerOptions,
   Trace,
 } from 'vscode-languageclient';
-
-import {Telemetry} from '../telemetry';
+import {Telemetry} from './telemetry';
 
 export class StripeLanguageClient {
-  static activate(
+  static async activate(
     context: vscode.ExtensionContext,
     serverOptions: ServerOptions,
     telemetry: Telemetry,
   ) {
     const outputChannel = vscode.window.createOutputChannel('Stripe Language Client');
+
+    // start the csharp server if this is a dotnet project
+    const dotnetProjectFile = await this.getDotnetProjectFiles();
+    if (dotnetProjectFile.length > 0) {
+      this.activateDotNetServer(context, outputChannel, dotnetProjectFile[0]);
+    } else {
+      this.activateUniversalServer(context, outputChannel, serverOptions, telemetry);
+    }
+  }
+
+  static activateUniversalServer(
+    context: vscode.ExtensionContext,
+    outputChannel: vscode.OutputChannel,
+    serverOptions: ServerOptions,
+    telemetry: Telemetry,
+  ) {
     outputChannel.appendLine('Starting universal client');
     const universalClientOptions: LanguageClientOptions = {
       // Register the server for stripe-supported languages. dotnet is not yet supported.
@@ -49,14 +63,12 @@ export class StripeLanguageClient {
     });
 
     universalClient.start();
-
-    // TODO: Check project contents before kicking this off.
-    this.activateDotNetServer(context, outputChannel);
   }
 
   static async activateDotNetServer(
     context: vscode.ExtensionContext,
     outputChannel: vscode.OutputChannel,
+    projectFile: string,
   ) {
     // TODO: replace this with real function that can find the executable
     const dotNetExecutable = '/usr/local/share/dotnet/dotnet';
@@ -66,7 +78,7 @@ export class StripeLanguageClient {
 
     const serverOptions: ServerOptions = {
       command: dotNetExecutable,
-      args: [serverAssembly],
+      args: [serverAssembly, projectFile],
     };
 
     // Options to control the language client
@@ -87,7 +99,6 @@ export class StripeLanguageClient {
         },
         closed: () => CloseAction.Restart,
       },
-      revealOutputChannelOn: RevealOutputChannelOn.Error,
     };
 
     // Create the language client and start the client.
@@ -108,5 +119,43 @@ export class StripeLanguageClient {
 
     await dotnetClient.onReady();
     outputChannel.appendLine('C# language service is running.');
+  }
+
+  /**
+   * Returns a solutions file or project file if it exists in the workspace.
+   *
+   * If the user is working with multiple workspaces that contain C# projects, we don't know which one to run the server on, so we'll return the first one we find.
+   * In the future, we may want to prompt the user to pick one, or expose a command that let's the user change the tracking project and restart.
+   *
+   * Returns null if none of the workspaces are .NET projects.
+   */
+  static async getDotnetProjectFiles(): Promise<string[]> {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      return [];
+    }
+
+    const projectFiles = await Promise.all(
+      workspaceFolders.map(async (w) => {
+        const workspacePath = w.uri.fsPath;
+
+        // First look for solutions files. We only expect one solutions file to be present in a workspace.
+        const pattern = new vscode.RelativePattern(workspacePath, '**/*.sln');
+        const sln = await vscode.workspace.findFiles(pattern, null, 1);
+        if (sln && sln.length === 1) {
+          return sln[0].fsPath;
+        } else {
+          // Check for csproj files. If there was no solutions file, return the csproj files.
+          // Note -- if there is no sln file there should be just one csproj file in the workspace.
+          const pattern = new vscode.RelativePattern(workspacePath, '**/*.csproj');
+          const csproj = await vscode.workspace.findFiles(pattern, null);
+          if (csproj && csproj.length === 1) {
+            return csproj[0].fsPath;
+          }
+        }
+      }),
+    );
+
+    return projectFiles.filter((file): file is string => Boolean(file));
   }
 }
