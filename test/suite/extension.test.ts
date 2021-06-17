919fd3d9bc30ff5c0576044191070dd16ffe1459
Grace Goo <gracegoo@stripe.com>
2021-06-17 13:36:59 -0700

    adding manual activation

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -3,6 +3,8 @@ import * as vscode from 'vscode';
 
 suite('Extension Test Suite', () => {
   test('Should start extension vscode-stripe', async () => {
+    const dotnetExtension = vscode.extensions.getExtension('ms-dotnettools.vscode-dotnet-runtime');
+    await dotnetExtension?.activate();
     const started = vscode.extensions.getExtension('stripe.vscode-stripe');
     assert.notStrictEqual(started, undefined);
     if (started) {
