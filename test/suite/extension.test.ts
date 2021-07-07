fa3333add9e257a376490ccc2176898b0318c72f
Grace Goo <gracegoo@stripe.com>
2021-07-07 13:32:39 -0700

    Manually installing extension before test

--- test/suite/extension.test.ts
+++ test/suite/extension.test.ts
@@ -1,15 +1,18 @@
-// import * as assert from 'assert';
-// import * as vscode from 'vscode';
+import * as assert from 'assert';
+import * as vscode from 'vscode';
 
 suite('Extension Test Suite', () => {
-  // test('Should start extension vscode-stripe', async () => {
-  //   const dotnetExtension = vscode.extensions.getExtension('ms-dotnettools.vscode-dotnet-runtime');
-  //   await dotnetExtension?.activate();
-  //   const started = vscode.extensions.getExtension('stripe.vscode-stripe');
-  //   assert.notStrictEqual(started, undefined);
-  //   if (started) {
-  //     await started.activate();
-  //   }
-  //   assert.strictEqual(started && started.isActive, true);
-  // });
+  test('Should start extension vscode-stripe', async () => {
+    // const dotnetExtension = vscode.extensions.getExtension('ms-dotnettools.vscode-dotnet-runtime');
+    // await dotnetExtension?.activate();
+    // console.log(dotnetExtension?.id);
+    // console.log('activated? ' + dotnetExtension?.isActive);
+    // console.log('Activated the dotnet extension');
+    const started = vscode.extensions.getExtension('stripe.vscode-stripe');
+    assert.notStrictEqual(started, undefined);
+    if (started) {
+      await started.activate();
+    }
+    assert.strictEqual(started && started.isActive, true);
+  });
 });
