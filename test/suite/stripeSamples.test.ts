d185676e0cc6230d69485156aba879c38f78131b
Evelyn Tsai <etsai@stripe.com>
2021-11-18 19:09:14 -0800

    spy withProgress

--- test/suite/stripeSamples.test.ts
+++ test/suite/stripeSamples.test.ts
@@ -128,6 +128,7 @@ suite('StripeSamples', function () {
         .stub(vscode.window, 'showInformationMessage')
         .resolves();
       sandbox.spy(vscode.env, 'openExternal');
+      sandbox.spy(vscode.window, 'withProgress');
 
       const stripeSamples = new StripeSamples(<any>stripeClient, <any>stripeDaemon);
 
